e4cc3faba0e82be35cd638a065127fb4f4fc34f9
Ayush Srivastava <ayush.cir@gmail.com>
2018-08-02 06:52:16 +0000

    initial try

--- twitter4j-core/src/main/java/twitter4j/api/TweetsResources.java
+++ twitter4j-core/src/main/java/twitter4j/api/TweetsResources.java
@@ -172,6 +172,19 @@ public interface TweetsResources {
      * Uploads media image to be attached via {@link #updateStatus(twitter4j.StatusUpdate)}
      * <br>This method calls https://api.twitter.com/1.1/media/upload.json
      *
+     * @param mediaFile the latest status to be updated.
+     * @return upload result
+     * @throws TwitterException when Twitter service or network is unavailable
+     * @see <a href="https://dev.twitter.com/docs/api/1.1/post/statuses/update">POST statuses/update | Twitter Developers</a>
+     * @see <a href="https://dev.twitter.com/docs/api/multiple-media-extended-entities">Multiple Media Entities in Statuses</a>
+     * @since Twitter4J 4.0.6
+     */
+     UploadedMedia uploadMediaChunked(File mediaFile) throws TwitterException;
+
+    /**
+     * Uploads media image to be attached via {@link #updateStatus(twitter4j.StatusUpdate)}
+     * <br>This method calls https://api.twitter.com/1.1/media/upload.json
+     *
      * @param fileName media file name
      * @param media media body as stream
      * @return upload result
